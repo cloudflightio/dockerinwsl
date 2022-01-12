fe5c41d39951880f9c5bcf98472f12d6c2f9474a
Michael Riedmann <michael.riedmann@cloudflight.io>
2022-01-12 17:39:20 +0100

    removed dynamic thumbprint selection from signing

--- msi/SignInstaller.ps1
+++ msi/SignInstaller.ps1
@@ -3,9 +3,7 @@ $signToolPath = Join-Path $winSdkFolder -ChildPath signtool.exe
 
 $pfxPassphrase = ($env:PFX_PASSPHRASE).Trim()
 $pfxPath = "$PSScriptRoot\..\Certificate.pfx"
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-$cert.Import(( Resolve-Path "$pfxPath"), $pfxPassphrase, "DefaultKeySet")
 
-$thumbprint = $cert.Thumbprint
+$thumbprint = ($env:PFX_THUMBPRINT).Trim()
 
 & $signToolPath sign /f "$pfxPath" /d "DockerInWSL" /p "$pfxPassphrase" /v /sha1 $thumbprint /t "http://timestamp.comodoca.com/authenticode" /fd SHA256 "$PSScriptRoot\Product.msi"

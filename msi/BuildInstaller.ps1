4d0b619b0150dea5cb6617b21806ded5a9f5187b
Michael Riedmann <michael.riedmann@cloudflight.io>
2022-01-26 13:55:42 +0100

    fixed local build problems with old obj files

--- msi/BuildInstaller.ps1
+++ msi/BuildInstaller.ps1
@@ -1,7 +1,7 @@
 $msbuild = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
 
 Push-Location $PSScriptRoot
-& $msbuild /p:Configuration=Release
+& $msbuild /p:Configuration=Release /t:"Clean,Build"
 
 if ($LASTEXITCODE -ne 0) {
     throw "Can't build msi"

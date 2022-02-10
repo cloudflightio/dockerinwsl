55af45f963291045dbd021cd548ce6cd47975437
Michael Riedmann <michael_riedmann@live.com>
2022-02-10 01:29:53 +0100

    added WSL Update check

--- msi/InstallScripts/check.ps1
+++ msi/InstallScripts/check.ps1
@@ -7,3 +7,7 @@ if(((wsl --help) | Measure-Object).Count -lt 300){
     [Microsoft.VisualBasic.Interaction]::MsgBox("WSL2 not installed!`n`nTry running 'wsl --install' in an elevated shell, reboot and start this install again.", 'OKOnly,SystemModal,Critical', "DockerInWSL Installation Error") | Out-Null
     throw "WSL2 not installed"
 }
+
+if ((Get-WmiObject Win32_Product | Where-Object { $_.Name -match 'Windows Subsystem for Linux Update' } | Measure-Object).Count -le 0) {
+    [Microsoft.VisualBasic.Interaction]::MsgBox("No 'Windows Subsystem for Linux Update' found!`nPlease visit https://docs.microsoft.com/en-us/windows/wsl/install-manual and install the update if this installation fails.") | Out-Null
+}

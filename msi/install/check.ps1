68589def3abe6acc7987d0254591cf82e2f883f2
Henrik Binggl <henrik@binggl.net>
2022-02-15 12:09:39 +0100

    feat(installer): use registry value to determine wsl version

--- msi/install/check.ps1
+++ msi/install/check.ps1
@@ -3,7 +3,8 @@ Set-StrictMode -Version 3
 . "$PSScriptRoot\_common.ps1"
 
 Add-Type -AssemblyName Microsoft.VisualBasic
-if(((wsl --help) | Measure-Object).Count -lt 150){
+$lxss = Get-ItemProperty -Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss"
+if($lxss -ne 2) {
     [Microsoft.VisualBasic.Interaction]::MsgBox("WSL2 not installed!`n`nTry running 'wsl --install' in an elevated shell, reboot and start this install again.", 'OKOnly,SystemModal,Critical', "DockerInWSL Installation Error") | Out-Null
     throw "WSL2 not installed"
 }

b903e450c8a6cae0a2f8ea35061df565da3b13f8
Michael Riedmann <michael_riedmann@live.com>
2022-02-15 15:56:29 +0100

    lowered WSL2 check threshold (fix #25)

--- msi/install/check.ps1
+++ msi/install/check.ps1
@@ -3,7 +3,7 @@ Set-StrictMode -Version 3
 . "$PSScriptRoot\_common.ps1"
 
 Add-Type -AssemblyName Microsoft.VisualBasic
-if(((wsl --help) | Measure-Object).Count -lt 300){
+if(((wsl --help) | Measure-Object).Count -lt 150){
     [Microsoft.VisualBasic.Interaction]::MsgBox("WSL2 not installed!`n`nTry running 'wsl --install' in an elevated shell, reboot and start this install again.", 'OKOnly,SystemModal,Critical', "DockerInWSL Installation Error") | Out-Null
     throw "WSL2 not installed"
 }

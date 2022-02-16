9f15265b632b1845610f95da4e4227f92e62d793
Michael Riedmann <michael_riedmann@live.com>
2022-02-16 01:06:29 +0100

    make signature optional if no secrets available during PR build

--- msi/SignInstaller.ps1
+++ msi/SignInstaller.ps1
@@ -1,3 +1,11 @@
+$ValidationErrors = @()
+if (-not (Test-Path env:PFX_THUMBPRINT)) { $ValidationErrors += @("PFX_THUMBPRINT envvar not set") }
+if (-not (Test-Path env:PFX_PASSPHRASE)) { $ValidationErrors += @("PFX_PASSPHRASE envvar not set") }
+if ($ValidationErrors.Count -gt 0) { 
+    Write-Warning "Validation Failed!`n$($ValidationErrors -join "`n")"
+    exit 0
+}
+
 $msbuild = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
 
 Push-Location $PSScriptRoot

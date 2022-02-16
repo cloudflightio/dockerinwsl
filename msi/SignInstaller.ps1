440c950db9f1bdbf7df953c5769657133d4327fa
Michael Riedmann <michael_riedmann@live.com>
2022-02-16 02:43:07 +0100

    sign installer pre-exec checks

--- msi/SignInstaller.ps1
+++ msi/SignInstaller.ps1
@@ -1,9 +1,9 @@
 $ValidationErrors = @()
-if (-not (Test-Path env:PFX_THUMBPRINT)) { $ValidationErrors += @("PFX_THUMBPRINT envvar not set") }
-if (-not (Test-Path env:PFX_PASSPHRASE)) { $ValidationErrors += @("PFX_PASSPHRASE envvar not set") }
+if (-not (Test-Path env:PFX_THUMBPRINT) -or (-not $env:PFX_THUMBPRINT)) { $ValidationErrors += @("PFX_THUMBPRINT envvar not set") }
+if (-not (Test-Path env:PFX_PASSPHRASE) -or (-not $env:PFX_THUMBPRINT)) { $ValidationErrors += @("PFX_PASSPHRASE envvar not set") }
 if ($ValidationErrors.Count -gt 0) { 
-    Write-Warning "Validation Failed!`n$($ValidationErrors -join "`n")"
-    exit 0
+    Write-Warning "`n$($ValidationErrors -join "`n")"
+    throw "Env-Vars not set!"
 }
 
 $msbuild = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe

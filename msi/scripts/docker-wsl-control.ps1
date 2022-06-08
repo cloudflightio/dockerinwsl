e457a60b9904037155a74782ff3bdff2034b9c5a
Michael Riedmann <michael_riedmann@live.com>
2022-06-08 11:02:35 +0200

    feat: add backup and restore commands to cli

--- msi/scripts/docker-wsl-control.ps1
+++ msi/scripts/docker-wsl-control.ps1
@@ -1,6 +1,6 @@
 param (
     [Parameter(Position = 0)]
-    [string]$Command,
+    [string]$Command="help",
     [Switch]$NoTranscript,
     [parameter(mandatory=$false, position=1, ValueFromRemainingArguments=$true)]$_
 )
@@ -9,6 +9,7 @@ $ScriptExitCode = 0
 $Transcript = -not $NoTranscript
 $TempDir = Join-Path $env:LOCALAPPDATA -ChildPath "Temp"
 $LogFilePath = Join-Path $TempDir -ChildPath "dockerinwsl.log"
+$LocalBase = "$env:LOCALAPPDATA\DockerInWSL"
 
 $SUPERVISOR_PID = "/run/supervisord.pid"
 $DISTRONAME = "clf_dockerinwsl"
@@ -74,6 +75,40 @@ function Get-CalledName {
     return ($callstack[1].Location -split ':')[0]
 }
 
+function Invoke-DockerEngineBackup {
+    Stop-Docker
+    Push-Location $LocalBase
+    & wsl -d "$DISTRONAME" -u root -- sh -c 'wsl-mount && tar -czpf backup.tar.gz /var/lib/docker'
+    if($LASTEXITCODE -ne 0) {
+        Write-Warning "Backup of existing WSL distro '$DISTRONAME' failed!"
+    } else {
+        Write-Host "WSL distro backup done"
+    }
+    Pop-Location
+    Start-Docker
+}
+
+function Invoke-DockerEngineRestore {
+    if (Test-Path ( Join-Path -Path $LocalBase -ChildPath "backup.tar.gz" )) {
+        Write-Host "Existing Docker backup found! Restoring ..."
+        Stop-Docker
+        Push-Location $LocalBase
+        & wsl -d "$DISTRONAME" -u root -- sh -c 'wsl-mount && cd / && tar -xpzf $(pwd)/backup.tar.gz -C /'
+        if($LASTEXITCODE -ne 0) {
+            Write-Warning "Restore Docker on WSL distro '$DISTRONAME' failed! Please inspect the backup-file at $LocalBase\backup.tar.gz and apply it manually to /var/lib/docker inside WSL"
+        }
+        Pop-Location
+        Start-Docker
+        Write-Host "Docker restored"
+    } else {
+        Write-Warning "Restore Docker on WSL distro '$DISTRONAME' failed! No Backup-File found at $LocalBase\backup.tar.gz"
+    }
+}
+
+function Write-CommandHelp {
+    "USAGE: ./$(Get-CalledName) ($($cmds.Keys -join '|'))" | Write-Host
+}
+
 $cmds = [ordered]@{
     "start"       = { Start-Docker }
     "stop"        = { Stop-Docker }
@@ -83,6 +118,9 @@ $cmds = [ordered]@{
     "enter-root"  = { Enter-DockerMachineAsRoot }
     "show-logs"   = { Show-Logs }
     "show-config" = { Show-ConfigFolder }
+    "restore"     = { Invoke-DockerEngineRestore }
+    "backup"      = { Invoke-DockerEngineBackup }
+    "help"        = { Write-CommandHelp }
 }
 
 try {
@@ -91,15 +129,14 @@ try {
     }
 
     if(-not ($Command -in $cmds.Keys)) {
-        throw "Unknown or missing command!"
+        throw "Unknown command!"
     }
-   "invoking $Command ..." |  Write-Output
     $cmds[$Command].invoke()
 }
 catch {
    "ERROR: $($Error[0])" | Write-Output 
     "" | Write-Output
-    "USAGE: ./$(Get-CalledName) ($($cmds.Keys -join '|'))" | Write-Output 
+    Write-CommandHelp
     $ScriptExitCode = 1
 }
 finally {
